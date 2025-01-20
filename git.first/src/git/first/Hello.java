71fe93b978b4ecd9dc767110784ccc9b27a5f4e7
David Levine <davidlevine.b@gmail.com>
2025-01-20 15:34:41 -0500

    Second commit

--- git.first/src/git/first/Hello.java
+++ git.first/src/git/first/Hello.java
@@ -3,7 +3,7 @@ package git.first;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello git; git is cool.");
+		System.out.println("Hello git; git is cooler than I thought.");
 
 	}
 
