3578e11ab22e3ecf7c6697abb7410a7704406191
davidlevine-b <davidlevine.b@gmail.com>
2025-01-26 11:55:08 -0500

    Added announcement

--- git.first/src/git/first/Hello.java
+++ git.first/src/git/first/Hello.java
@@ -3,7 +3,8 @@ package git.first;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("It will be a miracle if this works.");
+		System.out.println("Hear ye: I declare a miracle.");
+		System.out.println("Hello world");
 
 	}
 
