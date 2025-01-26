90004fb3f3b4c827ac5065b44ad4afbb5d542de7
davidlevine-b <davidlevine.b@gmail.com>
2025-01-26 12:06:19 -0500

    Try 2

--- git.first/src/git/first/Hello.java
+++ git.first/src/git/first/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hear ye: I declare a miracle.");
 		System.out.println("Hello world");
-		System.out.println("Goodbye");
+		System.out.println("Goodbye ");
 	}
 
 }
