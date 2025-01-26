8f8cd34fbb392993851e4f375e01940b4c1cd0d0
davidlevine-b <davidlevine.b@gmail.com>
2025-01-26 12:25:35 -0500

    First Alice

--- git.first/src/git/first/Hello.java
+++ git.first/src/git/first/Hello.java
@@ -3,7 +3,7 @@ package git.first;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("It will be a miracle if this works.");
+		System.out.println("Hello Alice");
 
 	}
 
